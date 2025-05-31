8ce4504274cbc7f547dabddc1b11bc0c3c6eb852
khale <khale@DESKTOP-3DANBIU>
2025-05-31 18:53:16 +0300

    Add Scope

---
+++ Ecom.Core/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using Ecom.Core.Entities.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.Core.Interfaces
+{
+    public interface ICategoryRepository:IGenericRepositry<Category>
+    {
+        //Task<IReadOnlyList<Category>> GetCategoriesWithProductsAsync();
+        //Task<Category> GetCategoryWithProductsAsync(int id);
+        //Task<IReadOnlyList<Category>> GetCategoriesWithProductsAsync(params string[] includes);
+        //Task<Category> GetCategoryWithProductsAsync(int id, params string[] includes);
+    }
+}
---
+++ Ecom.infrastructure/InfrastructureRegistration.cs
@@ -0,0 +1,27 @@
+﻿using Ecom.Core.Interfaces;
+using Ecom.infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.infrastructure
+{
+    public static class InfrastructureRegistration
+    {
+        public static IServiceCollection InfratructureConfiguration(this IServiceCollection services)
+        {
+            // Register your DbContext, Repositories, and other infrastructure services here
+            // Example: services.AddDbContext<AppDbContext>(options => options.UseSqlServer("YourConnectionString"));
+            
+            // services.AddScoped<IGenericRepositry<Category>, GenericRepositry<Category>>();
+            // services.AddScoped<IGenericRepositry<Product>, GenericRepositry<Product>>();
+            // services.AddScoped<IGenericRepositry<Photo>, GenericRepositry<Photo>>();
+            services.AddScoped(typeof(IGenericRepositry<>), typeof(GenericRepositry<>));
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            return services;
+        }
+    }
+}
